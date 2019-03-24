a685dac7f12e96dc178ab2aa498eeb0db76a52be
Stef Siekman <s.siekman@gmail.com>
2019-03-24 15:32:19 +0100

    Add random simulation to the nodes

--- node.py
+++ node.py
@@ -1,11 +1,28 @@
+from random import random, randint
+from typing import List
+
+MAX_CHILDREN = 2
+
+
 class Node:
 
     def __init__(self, parent=None):
         self.count = 0
         self.parent: Node = parent
+        self.children: List[Node] = [None] * MAX_CHILDREN
 
-    def simulate_random(self):
-        pass
+    def simulate(self):
+        # 1/4 chance of termination
+        if random() < .25:
+            self.back_propagate()
+            return
+
+        # Otherwise, continue with a random child (might have to be created)
+        child_nr = randint(0, MAX_CHILDREN - 1)
+        if not self.children[child_nr]:
+            self.children[child_nr] = Node(self)
+        self.children[child_nr].simulate()
+
 
     def back_propagate(self):
         self.count += 1
